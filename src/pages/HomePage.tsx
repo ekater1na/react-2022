82a113811f606696424df864bf66c3df5a2326f7
ekater1na <ekaterina_l@tut.by>
2022-10-02 17:02:36 +0300

    feat: add location

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { SearchBar } from '../components/SearchBar';
 import { Link } from 'react-router-dom';
 import { ProductList } from 'components/ProductList';
+import Location from 'components/Location';
 
 export function HomePage() {
   return (
     <div>
-      <SearchBar />
-
       <nav
         style={{
-          borderBottom: 'solid 1px',
-          paddingBottom: '1rem',
+          borderBottom: 'solid 1px #87CEEB',
+          paddingBottom: '2px',
+
+          paddingTop: '2px',
+          margin: '2px 0',
         }}
       >
         <Link to="/about">About Us</Link>
       </nav>
+
+      <Location />
+
+      <SearchBar />
 
       <ProductList />
     </div>
