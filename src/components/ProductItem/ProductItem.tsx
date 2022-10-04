3c0ddeb22616eaa237712e3b082ae4da80a2e2cd
ekater1na <ekaterina_l@tut.by>
2022-10-04 19:14:11 +0300

    fix: fix prettier error

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -18,9 +18,7 @@ export function ProductItem({ product }: ProductProps) {
         <p className="text-gray-400">{product.category}</p>
         <p className="font-bold text-xl">{product.price}$</p>
       </div>
-      <a
-        className="px-6 py-2 text-sm font-semibold text-blue-600 bg-blue-100 hover:bg-indigo-400 focus:bg-indigo-600 focus:outline-none"
-      >
+      <a className="px-6 py-2 text-sm font-semibold text-blue-600 bg-blue-100 hover:bg-indigo-400 focus:bg-indigo-600 focus:outline-none">
         Show details
       </a>
     </div>
