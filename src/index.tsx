63bb7a5911f5e54865785944b3fdc936e8cdd69a
ekater1na <ekaterina_l@tut.by>
2022-10-30 14:35:14 +0300

    feat: remove baseurl

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter basename="/ekater1na-REACT2022Q3">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
