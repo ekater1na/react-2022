aeef4276251ef4f74bbf2453adee5818cb9ff479
ekater1na <ekaterina_l@tut.by>
2022-10-30 15:48:53 +0300

    feat: add env variable

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -9,8 +9,8 @@ export function useCharacters(searchValue: string | number) {
   const [error, setError] = useState('');
 
   const fetchCharacters = async (searchValue: string | number) => {
-    const URL = process.env.URL
-      ? process.env.URL
+    const URL = process.env.API_URL
+      ? process.env.API_URL
       : 'https:/rickandmortyapi.com/api/character/?name=';
     try {
       setError('');
