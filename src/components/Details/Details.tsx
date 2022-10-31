5933f7ba969a01f99569f2ee49b87d29cc8aa419
ekater1na <ekaterina_l@tut.by>
2022-11-01 01:12:09 +0300

    feat: update detail page

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -21,39 +21,41 @@ export function Details() {
   return (
     <>
       <div
-        className="px-6 rounded w-[500px] p-5 bg-white absolute top-24 left-1/2 -translate-x-1/2"
+        className="px-6 rounded w-[800px] p-2 bg-white absolute top-24 left-1/2 -translate-x-1/2"
         data-testid="details"
       >
-        <Link className="bg-blue-200 p-2" to="/">
+        <Link className="bg-red-300 p-2 rounded-xl" to="/">
           Back Home
         </Link>
-        <h1 className="text-2xl text-center mb-2 text-gray-800 font-bold">{title}</h1>
+        <div className="bg-gray-300 mt-4 mb-2 py-1 rounded-xl">
+          <h1 className="text-3xl text-center mb-2 text-gray-800 font-bold">{title}</h1>
+        </div>
         <section className="container mx-auto bg-gradient-to-r from-blue-900 to-blue-500 p-4 py-5 px-5 rounded-xl">
           <div className="flex items-center justify-between py-3">
-            <p className="text-sm text-white mt-[3px]">Episodes: {character?.episode.length}</p>
-            <p className="text-sm text-white mt-[3px]">ID: {id}</p>
+            <p className="text-m text-white mt-[3px]">Episodes: {character?.episode.length}</p>
+            <p className="text-m text-white mt-[3px]">ID: {id}</p>
           </div>
-          <div className="my-1 py-2 px-6 rounded flex flex-col items-center mb-2 h-80">
-            <div className="h-1/2 flex justify-center">
+          <div className="my-1 py-2 px-6 rounded flex flex-col items-center mb-2">
+            <div className="h-[300px] flex justify-center">
               <img
                 src={character?.image}
                 alt={character?.image}
                 className="border-double border-4 border-white"
               />
             </div>
-            <div className="w-full">
-              <p className="text-center truncate text-red-400 text-xl font-bold">
+            <div className="w-full py-4">
+              <p className="text-center truncate text-red-400 text-3xl font-bold">
                 {character?.name}
               </p>
               <a href={character?.origin.url} target="_blank" rel="noreferrer">
-                <p className="text-center truncate text-sm px-2" title="origin name">
+                <p className="text-center truncate text-xl px-2" title="origin name">
                   {character?.origin.name}
                 </p>
               </a>
             </div>
-            <p className="text-gray-400 text-sm">{character?.species}</p>
+            <p className="text-gray-400 text-m">{character?.species}</p>
 
-            <div className="px-1 py-2">
+            <div className="px-1 py-4 text-xl">
               <div className="flex items-center justify-between">
                 <span className="text-white px-2">
                   {'📆'}
@@ -80,11 +82,11 @@ export function Details() {
                   <FontAwesomeIcon icon={['fas', 'check-square']} />
                 </span>
               </div>
-              <div className="text-center">
-                <p className="text-sm text-blue-200 mt-[3px]">
+              <div className="text-center py-2">
+                <p className="text-m text-blue-200 mt-[3px]">
                   Location: {character?.location.name}
                 </p>
-                <button className="bg-white hover:bg-white-500 text-blue-700 font-semibold hover:text-blue-900 my-4 py-1 px-4 border border-blue-500 hover:border-transparent rounded">
+                <button className=" mt-16 bg-white hover:bg-white-500 text-blue-700 font-semibold hover:text-blue-900 my-4 py-1 px-4 border border-blue-500 hover:border-transparent rounded">
                   <a href={character?.url} target="_blank" rel="noreferrer">
                     Visit page
                   </a>
